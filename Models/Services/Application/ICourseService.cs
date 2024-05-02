81071130262fbc6f0f6611fc4ad5b646f7b523f6
Pablito98 <giorgiavalenti97@gmail.com>
2024-05-02 10:50:25 +0200

    aggiornamento versione

--- Models/Services/Application/ICourseService.cs
+++ Models/Services/Application/ICourseService.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using MyCourse.Models.ViewModels;
-using System.Threading.Tasks;
 
 namespace MyCourse.Models.Services.Application
 {
--- Startup.cs
+++ Startup.cs
@@ -22,7 +22,7 @@ namespace MyCourse
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc();
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
 
             //registrazione del servizio di dependency a injection:
            // services.AddTransient<ICourseService, CourseService>(); 
